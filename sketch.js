710058ce167401f0e0176927a67907d33f7b9e59
PlagueyPlatypus <114016382+PlagueyPlatypus@users.noreply.github.com>
2022-12-14 12:00:46 -0600

    canvas small edit

--- sketch.js
+++ sketch.js
@@ -5,11 +5,20 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+class Brush {
+  constructor() {
+    this.x = mouseX;
+    this.y = mouseY;
+    this.px = pwinMouseX;
+    this.py = pwinMouseY;
+  }
+}
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  let canvas = createCanvas(windowWidth/2, windowHeight);
+  canvas.center("horizontal");
 }
 
 function draw() {
-  background(220);
+  background("white");
 }
