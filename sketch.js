fc8d31f22ea0097596c4524473cb1de222aaa661
Islam4403328 <4403328@sps-student.sk.ca>
2022-12-16 11:08:06 -0600

    Addition of Brush Icon

--- sketch.js
+++ sketch.js
@@ -5,35 +5,87 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-class Brush {
-  constructor() {
-    this.x = winMouseX;
-    this.y = winMouseY;
-    this.px = pwinMouseX;
-    this.py = pwinMouseY;
-  }
-
-  show() {
-    stroke(0);
-    line(this.x, this.y, this.px, this.py);
-  }
+function preload() {
+  fishimage = loadImage("brush.png");
 }
 
-let lines = []
+//brush object
+// class Brush {
+//   constructor() {
+//     this.x = winMouseX;
+//     this.y = winMouseY;
+//     this.px = pwinMouseX;
+//     this.py = pwinMouseY;
+//   }
+
+//   show() {
+//     stroke(0);
+//     line(this.x, this.y, this.px, this.py);
+//   }
+// }
+
+// let lines = []
 
 function setup() {
-  let canvas = createCanvas(windowWidth/2, windowHeight);
+  let canvas = createCanvas(windowWidth, windowHeight);
   canvas.center("horizontal");
+  imageMode(CENTER);
 }
 
-function mousePressed() {
-  let line = new Brush()
-  lines.push(line);
-  for (let i = 0; i < lines; i++) {
-    line.show();
+//brush show details
+// function mousePressed() {
+//   let line = new Brush()
+//   lines.push(line);
+//   for (let i = 0; i < lines; i++) {
+//     line.show();
+//   }
+// }
+
+//testing new scripts for colour gradient
+function createColorPicker() {
+  colorPicker = createImage(100, height);
+  let myWidth = colorPicker.width/3
+  colorPicker.loadPixels()
+  from = color(0, 255, 0);
+  to = color(255, 0, 0);
+  for (let y = 0; y < height; y++) {
+    for (x = 0; x < myWidth; x++) {
+      color1 = lerpColor(from, to, y / height)
+      colorPicker.set(x, y, color1)
+    }
   }
+  from = color(0, 0, 255);
+  to = color(0, 255, 0);
+  for (let y = 0; y < height; y++) {
+    for (x = myWidth; x < myWidth* 2; x++) {
+      color1 = lerpColor(from, to, y / height)
+      colorPicker.set(x, y, color1)
+    }
+  }
+  from = color(255, 0, 0);
+  to = color(0, 255, 255);
+  for (let y = 0; y < height; y++) {
+    for (x = myWidth*2; x < myWidth * 3; x++) {
+      color1 = lerpColor(from, to, y / height)
+      colorPicker.set(x, y, color1)
+    }
+  }
+  colorPicker.updatePixels()
+  image(colorPicker, 400, 0)
 }
+
+// function draw() {
+//   background("white");
+//   mousePressed();
+// }
+
 
 function draw() {
-  background("white");
+  background(220);
+  //cursor
+  image(brushimage, mouseX, mouseY, fishimage.width*scalar, fishimage.height*scalar);
+  noCursor();
+
+  //gradiance scale
+  radius = slider.value();
 }
